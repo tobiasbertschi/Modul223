5066d9932047521fd6d091802a3c23195f2a42b2
tobiasbertschi <gotteron89tb@gmail.com>
2022-11-23 13:14:28 +0000

    Tests für Platz gemacht

--- src/test/java/ch/zli/m223/PlatzRessourceTest.java
+++ src/test/java/ch/zli/m223/PlatzRessourceTest.java
@@ -1,5 +1,49 @@
 package ch.zli.m223;
 
+import org.junit.jupiter.api.Test;
+
+import ch.zli.m223.model.Platz;
+import ch.zli.m223.model.User;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.security.TestSecurity;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+
+import java.util.Date;
+
+@QuarkusTest
 public class PlatzRessourceTest {
-    
+
+    @Test
+    @TestSecurity(user = "role", roles = "Admin")
+    public void testGetPlatzEndpoint() {
+        given()
+                .when().get("/platz")
+                .then()
+                .statusCode(200);
+
+    }
+
+    @Test
+    @TestSecurity(user = "role", roles = "Mitglied")
+    public void testCreatePlatzEndpoint() {
+
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\"stehpult\":\"true\",\"raum\":{\"id\":2}}")
+                .when().get("/platz/")
+                .then()
+                .statusCode(403);
+    }
+
+    @Test
+    @TestSecurity(user = "role", roles = "Admin")
+    public void testDeletePlatzEndpoint() {
+        given()
+                .when().delete("/platz/2")
+                .then()
+                .statusCode(204);
+    }
+
 }
